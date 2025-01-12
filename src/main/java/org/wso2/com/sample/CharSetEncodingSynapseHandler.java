1f62eefd17b0d400d065b0c4443c20571fd2eccd
sajithmadhusanka <madhusanka@wso2.com>
2025-01-12 22:18:09 +0530

    improve logging

--- src/main/java/org/wso2/com/sample/CharSetEncodingSynapseHandler.java
+++ src/main/java/org/wso2/com/sample/CharSetEncodingSynapseHandler.java
@@ -17,23 +17,35 @@ public class CharSetEncodingSynapseHandler extends AbstractSynapseHandler {
         org.apache.axis2.context.MessageContext axis2context =
                 ((Axis2MessageContext) context).getAxis2MessageContext();
 
+        if (log.isDebugEnabled()) {
+            log.debug("Starting handleRequestInFlow for message context: " + axis2context.getMessageID());
+        }
+
         /*
          * Check if the character set encoding (CHARACTER_SET_ENCODING) is specified
          * in the Axis2 message context. If the character set encoding is present in the Content-Type header,
          * it should be stored in the Axis2 message context under the CHARACTER_SET_ENCODING property.
          */
-        String charSetEnc = (String) axis2context
-                .getProperty(Constants.Configuration.CHARACTER_SET_ENCODING);
+        String charSetEnc = (String) axis2context.getProperty(Constants.Configuration.CHARACTER_SET_ENCODING);
+
+
 
         /*
          * If CHARACTER_SET_ENCODING is not found in the message context,
          * attempt to retrieve it from the operation context.
          */
         if (charSetEnc == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("CHARACTER_SET_ENCODING not found in the Axis2 Message Context");
+            }
             OperationContext opctx = axis2context.getOperationContext();
             if (opctx != null) {
-                charSetEnc = (String) opctx
-                        .getProperty(Constants.Configuration.CHARACTER_SET_ENCODING);
+                charSetEnc = (String) opctx.getProperty(Constants.Configuration.CHARACTER_SET_ENCODING);
+                if (log.isDebugEnabled()) {
+                    log.debug("CHARACTER_SET_ENCODING retrieved from operation context: " + charSetEnc);
+                }
+            } else if (log.isDebugEnabled()) {
+                log.debug("Operation context is null. Unable to retrieve CHARACTER_SET_ENCODING from it.");
             }
 
             /*
@@ -44,6 +56,9 @@ public class CharSetEncodingSynapseHandler extends AbstractSynapseHandler {
              */
             if (charSetEnc == null) {
                 charSetEnc = (String) axis2context.getProperty(Constants.Configuration.XML_DECLARATION_ENCODING);
+                if (log.isDebugEnabled()) {
+                    log.debug("CHARACTER_SET_ENCODING retrieved from XML_DECLARATION_ENCODING: " + charSetEnc);
+                }
             }
 
             /*
@@ -52,14 +67,27 @@ public class CharSetEncodingSynapseHandler extends AbstractSynapseHandler {
              */
             if (charSetEnc == null) {
                 charSetEnc = org.apache.axis2.context.MessageContext.DEFAULT_CHAR_SET_ENCODING;
+                if (log.isDebugEnabled()) {
+                    log.debug("CHARACTER_SET_ENCODING not found. Using default encoding: " + charSetEnc);
+                }
             }
 
             // Set the determined character set encoding in the Axis2 message context.
             axis2context.setProperty(Constants.Configuration.CHARACTER_SET_ENCODING, charSetEnc);
+            if (log.isDebugEnabled()) {
+                log.debug("CHARACTER_SET_ENCODING set in Axis2 message context: " + charSetEnc);
+            }
+        } else if (log.isDebugEnabled()) {
+            log.debug("CHARACTER_SET_ENCODING already present in Axis2 message context: " + charSetEnc);
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Completed handleRequestInFlow for message context: " + axis2context.getMessageID());
         }
 
         return true;
     }
+
 
     @Override
     public boolean handleRequestOutFlow(MessageContext messageContext) {
